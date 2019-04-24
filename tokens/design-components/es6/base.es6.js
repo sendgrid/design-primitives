88fb52e6577d9905bd6fbdfea393ab8243fb11eb
richbachman <rb@richbachman.com>
2019-04-24 15:26:59 -0600

    revising banner message tokens.

--- tokens/design-components/es6/base.es6.js
+++ tokens/design-components/es6/base.es6.js
@@ -3,14 +3,14 @@
  * SendGrid Design Tokens
  * 
  */
+export const bannerMessageBackgroundColor = "rgba(26, 130, 226, 0.05)";
 export const bannerMessageBetaBackground = "rgba(124, 77, 255, 0.05)";
 export const bannerMessageBetaBorderColor = "rgb(124, 77, 255)";
+export const bannerMessageBorderColor = "rgb(26, 130, 226)";
 export const bannerMessageBorderType = "solid";
 export const bannerMessageBorderWidth = "2px";
 export const bannerMessageDisplay = "inline-block";
 export const bannerMessageFontSize = "13px";
-export const bannerMessageInfoBackground = "rgba(26, 130, 226, 0.05)";
-export const bannerMessageInfoBorderColor = "rgb(26, 130, 226)";
 export const bannerMessagePadding = "9px 12px";
 export const bannerMessageTextAlign = "left";
 export const colfax = "\"Colfax\", Helvetica, Arial, sans-serif";
@@ -18,14 +18,14 @@ export const colfaxBold = "\"Colfax-Bold\", Helvetica, Arial, sans-serif";
 export const colfaxLight = "\"Colfax-Light\", Helvetica, Arial, sans-serif";
 export const colfaxMedium = "\"Colfax-Medium\", Helvetica, Arial, sans-serif";
 export const bannerMessage = {
+bannerMessageBackgroundColor,
 bannerMessageBetaBackground,
 bannerMessageBetaBorderColor,
+bannerMessageBorderColor,
 bannerMessageBorderType,
 bannerMessageBorderWidth,
 bannerMessageDisplay,
 bannerMessageFontSize,
-bannerMessageInfoBackground,
-bannerMessageInfoBorderColor,
 bannerMessagePadding,
 bannerMessageTextAlign,
 };
