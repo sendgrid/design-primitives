b246396aa08b5f0cf00ae649facf51fe1607f988
richbachman <rb@richbachman.com>
2019-05-03 10:26:14 -0600

    new colors. refine sg-dot-com es6.

--- tokens/sg-dot-com/es6/base.es6.js
+++ tokens/sg-dot-com/es6/base.es6.js
@@ -7,9 +7,179 @@ export const breakpointDesktop = "1280px";
 export const breakpointMobile = "768px";
 export const breakpointSmall = "520px";
 export const breakpointTablet = "992px";
-export const breakpoints = {
+export const colorBlack = "#000";
+export const colorBlue = "#3368fa";
+export const colorBlueDark = "#002ca6";
+export const colorBlueDarkest = "#000e35";
+export const colorBlueLight = "#baccff";
+export const colorBlueLightest = "#f5f8ff";
+export const colorGray100 = "#ebf2ff";
+export const colorGray200 = "#dfe9fa";
+export const colorGray300 = "#c8d7ee";
+export const colorGray400 = "#a8b9d5";
+export const colorGray50 = "#f5f8fd";
+export const colorGray500 = "#8ba3ca";
+export const colorGray600 = "#546b91";
+export const colorGray700 = "#2c4a7d";
+export const colorGray800 = "#0b2c64";
+export const colorGray900 = "#00173e";
+export const colorGreen = "#23bf6e";
+export const colorGreenDark = "#006631";
+export const colorGreenDarkest = "#00361a";
+export const colorGreenLight = "#8eeebc";
+export const colorGreenLightest = "#e0ffef";
+export const colorOrange = "#f28510";
+export const colorOrangeDark = "#a83e00";
+export const colorOrangeDarkest = "#762e04";
+export const colorOrangeLight = "#fbb68f";
+export const colorOrangeLightest = "#ffecd8";
+export const colorPink = "#f772a2";
+export const colorPinkDark = "#d82a68";
+export const colorPinkDarkest = "#7c002c";
+export const colorPinkLight = "#ffcadd";
+export const colorPinkLightest = "#ffeff5";
+export const colorPurple = "#6f3cca";
+export const colorPurpleDark = "#400e9a";
+export const colorPurpleDarkest = "#1c0050";
+export const colorPurpleLight = "#dbc7ff";
+export const colorPurpleLightest = "#f3edff";
+export const colorRed = "#ce241a";
+export const colorRedDark = "#740600";
+export const colorRedDarkest = "#4a0400";
+export const colorRedLight = "#ff8c85";
+export const colorRedLightest = "#ffe9e7";
+export const colorTeal = "#22d5a7";
+export const colorTealDark = "#00634a";
+export const colorTealDarkest = "#001611";
+export const colorTealLight = "#96fde2";
+export const colorTealLightest = "#dbfff6";
+export const colorTurquoise = "#199dff";
+export const colorTurquoiseDark = "#006cbc";
+export const colorTurquoiseDarkest = "#00345a";
+export const colorTurquoiseLight = "#b9e1ff";
+export const colorTurquoiseLightest = "#ebf7ff";
+export const colorViolet = "#c850de";
+export const colorVioletDark = "#740089";
+export const colorVioletDarkest = "#490057";
+export const colorVioletLight = "#f3b1ff";
+export const colorVioletLightest = "#fdf1ff";
+export const colorWhite = "#fff";
+export const colorYellow = "#fabb2b";
+export const colorYellowDark = "#996b00";
+export const colorYellowDarkest = "#704e00";
+export const colorYellowLight = "#ffde92";
+export const colorYellowLightest = "#fff7e5";
+export const fontWeightBold = "600";
+export const fontWeightLight = "300";
+export const fontWeightMedium = "500";
+export const fontWeightNormal = "400";
+export const scaleBase = "16px";
+export const scaleDown01 = "14px";
+export const scaleDown02 = "13px";
+export const scaleDown03 = "8px";
+export const scaleDown04 = "4px";
+export const scaleDown05 = "2px";
+export const scaleUp01 = "18px";
+export const scaleUp02 = "20px";
+export const scaleUp03 = "24px";
+export const scaleUp04 = "32px";
+export const scaleUp05 = "48px";
+export const scaleUp06 = "64px";
+export const scaleUp07 = "72px";
+export const scaleUp08 = "80px";
+export const scaleUp09 = "88px";
+export const scaleUp10 = "96px";
+export const breakpoint = {
 breakpointDesktop,
 breakpointMobile,
 breakpointSmall,
 breakpointTablet,
+};
+export const color = {
+colorBlack,
+colorBlue,
+colorBlueDark,
+colorBlueDarkest,
+colorBlueLight,
+colorBlueLightest,
+colorGray100,
+colorGray200,
+colorGray300,
+colorGray400,
+colorGray50,
+colorGray500,
+colorGray600,
+colorGray700,
+colorGray800,
+colorGray900,
+colorGreen,
+colorGreenDark,
+colorGreenDarkest,
+colorGreenLight,
+colorGreenLightest,
+colorOrange,
+colorOrangeDark,
+colorOrangeDarkest,
+colorOrangeLight,
+colorOrangeLightest,
+colorPink,
+colorPinkDark,
+colorPinkDarkest,
+colorPinkLight,
+colorPinkLightest,
+colorPurple,
+colorPurpleDark,
+colorPurpleDarkest,
+colorPurpleLight,
+colorPurpleLightest,
+colorRed,
+colorRedDark,
+colorRedDarkest,
+colorRedLight,
+colorRedLightest,
+colorTeal,
+colorTealDark,
+colorTealDarkest,
+colorTealLight,
+colorTealLightest,
+colorTurquoise,
+colorTurquoiseDark,
+colorTurquoiseDarkest,
+colorTurquoiseLight,
+colorTurquoiseLightest,
+colorViolet,
+colorVioletDark,
+colorVioletDarkest,
+colorVioletLight,
+colorVioletLightest,
+colorWhite,
+colorYellow,
+colorYellowDark,
+colorYellowDarkest,
+colorYellowLight,
+colorYellowLightest,
+};
+export const fontWeight = {
+fontWeightBold,
+fontWeightLight,
+fontWeightMedium,
+fontWeightNormal,
+};
+export const scale = {
+scaleBase,
+scaleDown01,
+scaleDown02,
+scaleDown03,
+scaleDown04,
+scaleDown05,
+scaleUp01,
+scaleUp02,
+scaleUp03,
+scaleUp04,
+scaleUp05,
+scaleUp06,
+scaleUp07,
+scaleUp08,
+scaleUp09,
+scaleUp10,
 };
